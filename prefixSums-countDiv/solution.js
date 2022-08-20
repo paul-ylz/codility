1d139adcb1690af94a189c2d58c953e2d1b52b44
Paul Yeoh <paulyeoh.lz@gmail.com>
2022-08-20 20:50:02 +0800

    prefixSums - countDiv, 100%, O(1)

--- prefixSums-countDiv/solution.js
+++ prefixSums-countDiv/solution.js
@@ -1,12 +1,15 @@
+//Given:
+//A and B are integers within the range [0..2,000,000,000];
+//K is an integer within the range [1..2,000,000,000];
+//A ≤ B.
+//
+// we want the number of times i%K === 0 from 0..B minus
+// number of times i%K === 0 from 0..A
+//
 function solution(A, B, K) {
-  let divisibleCount = 0;
-  for (let i = A; i <= B; i++) {
-    if (i % K === 0) {
-      divisibleCount ++;
-    }
-  }
-
-  return divisibleCount;
+  const divCountA = Math.floor((A-1) / K); // 2
+  const divCountB = Math.floor(B / K); // 5
+  return divCountB - divCountA;
 }
 
 module.exports = solution;
--- prefixSums-countDiv/solution.test.js
+++ prefixSums-countDiv/solution.test.js
@@ -7,4 +7,11 @@ describe('CountDiv', () => {
     const K = 2;
     expect(solution(A,B,K)).toBe(3);
   });
+
+  test('simple', () => {
+    const A = 11;
+    const B = 345;
+    const K = 17;
+    expect(solution(A,B,K)).toBe(20);
+  });
 });
