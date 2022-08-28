1404df3deceb930f231dc3a859e94b2c89969313
Paul Yeoh <paulyeoh.lz@gmail.com>
2022-08-28 14:53:39 +0800

    sorting - maxProductOfThree. 100%, O(N log(n))

--- sorting-maxProductOfThree/solution.js
+++ sorting-maxProductOfThree/solution.js
@@ -1,16 +1,48 @@
+//
 // Given:
 // N is an integer within the range [3..100,000];
 // each element of array A is an integer within the range [−1,000..1,000].
 //
+// Possible combinations:
+// 1. All positive and > 0
+// Take the 3 biggest numbers
+//
+// 2. 1 negative, 2 positive [ -2, 0, 1, 2]
+// Take the smallest numbers
+//
+// 3. 2 negative, 1 positive [-3, -2, 0, 1, 2, 4]
+// Take biggest absolute numbers
+//
+// Calling sort() will cause performance to fail.
+//
 // solution returns the product of the maximal triplet to be found in array A.
 function solution(A) {
-  A.sort()
+  // do a count sort first
   const N = A.length
-  const large1 = A[N-1]
-  const large2 = A[N-2]
-  const large3 = A[N-3]
+  let count = new Array(2001).fill(0)
+  const rangeOffset = 1000
 
-  return large1 * large2 * large3
+  // count tracks counts for numbers in the range [-1000..1000]
+  for (let i=0; i<N; i++) {
+    count[A[i]+rangeOffset]++
+  }
+
+  let p = 0
+  for (let i=0; i<2001; i++) {
+    for (let j=0; j < count[i]; j++) {
+      A[p] = i - rangeOffset
+      p++
+    }
+  }
+
+  const allPositiveMax = A[N-1] * A[N-2] * A[N-3]
+
+  let twoNegativesMax = 0
+  if (A[0] < 0 && A[1] < 0) {
+    twoNegativesMax = A[0] * A[1] * A[N-1]
+  }
+
+  return Math.max(allPositiveMax, twoNegativesMax)
 }
 
 module.exports = solution
--- sorting-maxProductOfThree/solution.test.js
+++ sorting-maxProductOfThree/solution.test.js
@@ -12,4 +12,22 @@ describe('sorting - maxProductOfThree', () => {
 
     expect(solution(A)).toEqual(1000)
   });
+
+  test('negatives are bigger', () => {
+    const A = [-2, -3, 0, 1, 2, 4]
+
+    expect(solution(A)).toEqual(24)
+  });
+
+  test('zeroes do not contribute to max', () => {
+    const A = [-3, 0, 2, 4]
+
+    expect(solution(A)).toEqual(0)
+  });
+
+  test('negatives are smaller', () => {
+    const A = [-2, -1, 0, 1, 2, 4]
+
+    expect(solution(A)).toEqual(8)
+  });
 });
